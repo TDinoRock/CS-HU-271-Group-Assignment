902328ee8c5d589353791d2eec560e84075dd8d7
Robbie Miller <robbiemiller928@u.boisestate.edu>
2022-06-29 15:48:45 -0600

    This is the scalar multiplication method and test method.

--- src/main/java/com/cs271/Matrix.java
+++ src/main/java/com/cs271/Matrix.java
@@ -1,6 +1,7 @@
 package com.cs271;
 
 
+import javax.lang.model.type.NullType;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -28,6 +29,14 @@ public class Matrix {
     public void print() {
     }
 
+    public int numberOfColumns() {
+        return this.matrix[0].length;
+    }
+
+    public int numberOfRows() {
+        return this.matrix.length;
+    }
+
 
     public static Matrix add(Matrix a, Matrix b) throws InvalidMatrixOperation {
         return  new Matrix();
@@ -50,7 +59,19 @@ public class Matrix {
     }
 
     public static Matrix multiply(int c, Matrix a) {
-        return new Matrix();
+        if (a == null) {
+            throw new NullPointerException("The matrix is null");
+        }
+
+            Matrix scalarMultiply = new Matrix();
+            int[][] smMatrix = a.to2DArray();
+            for (int rows = 0; rows < smMatrix.length; rows++) {
+                for (int cols = 0; cols < smMatrix[rows].length; cols++) {
+                    smMatrix[rows][cols] = c * smMatrix[rows][cols];
+                }
+            }
+            scalarMultiply.load(smMatrix);
+            return scalarMultiply;
     }
 
     public static Matrix transposition(Matrix a) {
@@ -59,6 +80,41 @@ public class Matrix {
 
     public static int determinate(int c, Matrix a) {
         return 0;
+    }
+
+    public static Boolean isEqual(Matrix a, Matrix b) throws InvalidMatrixOperation {
+        if (a == null || b == null) {
+            throw new NullPointerException("One or both of the matrices are null");
+        }
+
+        int[][] firstMatrix = a.to2DArray();
+        int[][] secondMatrix = b.to2DArray();
+
+        if (firstMatrix.length != secondMatrix.length) {
+            throw new InvalidMatrixOperation("Error: The matrices are different sizes (rows)");
+        }
+
+        if (firstMatrix.length == 0 && secondMatrix.length == 0) {}
+        else {
+            if (firstMatrix.length > 0 && secondMatrix.length > 0) {
+                for (int row = 0; row < firstMatrix.length; row++) {
+                    if (firstMatrix[row].length != secondMatrix[row].length) {
+                        throw new InvalidMatrixOperation("Error: The matrices are different sizes (columns)");
+                    }
+                }
+            }
+        }
+
+        for (int row = 0; row < firstMatrix.length; row++) {
+            for (int column = 0; column < firstMatrix[row].length; column++) {
+                if (firstMatrix[row][column] != secondMatrix[row][column]) {
+                    throw new InvalidMatrixOperation("Error: Matrices contain different values " +
+                                    firstMatrix[row][column] + "!=" + secondMatrix[row][column]);
+                }
+            }
+        }
+
+        return true;
     }
 
 }
--- src/test/java/com/cs271/MatrixTest.java
+++ src/test/java/com/cs271/MatrixTest.java
@@ -3,6 +3,9 @@ package com.cs271;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static com.cs271.Matrix.isEqual;
+import static com.cs271.Matrix.multiply;
+
 class MatrixTests {
 
 
@@ -14,9 +17,197 @@ class MatrixTests {
         Assertions.assertArrayEquals(expected, a.to2DArray());
 
 
-
         int[][] expected2 = {{1}};
         a.load(expected2);
         Assertions.assertArrayEquals(expected2, a.to2DArray());
+    }
+
+
+    @Test
+    void scalarMultiplicationTests() throws NullPointerException {
+        System.out.println("\n===========================================");
+        System.out.println("Tests for Scalar Multiplication Method");
+
+        Matrix testMatrix = new Matrix();
+        int multiply = 2;
+
+        int[][] testMatrix1 = {{1}};
+        int[][] testMatrix2 = {{1, 1}};
+        int[][] testMatrix3 = {{1}, {2}};
+        int[][] testMatrix4 = {{1, 1}, {2, 2}};
+        int[][] testMatrix5 = {{1}, {2, 2}, {3, 3, 3}};
+        int[][] testMatrix6 = {{1, 1}, {2, 2}, {3, 3}};
+        int[][] testMatrix7 = {{1, 1, 1}, {2, 2, 2}};
+        int[][] testMatrix8 = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+
+        Matrix expectedMatrix = new Matrix();
+        int[][] expectedMatrix1 = {{2}};
+        int[][] expectedMatrix2 = {{2, 2}};
+        int[][] expectedMatrix3 = {{2}, {4}};
+        int[][] expectedMatrix4 = {{2, 2}, {4, 4}};
+        int[][] expectedMatrix5 = {{2}, {4, 4}, {6, 6, 6}};
+        int[][] expectedMatrix6 = {{2, 2}, {4, 4}, {6, 6}};
+        int[][] expectedMatrix7 = {{2, 2, 2}, {4, 4, 4}};
+        int[][] expectedMatrix8 = {{2, 2, 2}, {4, 4, 4}, {6, 6, 6}};
+
+        Matrix finalMult;
+
+        testMatrix.load(testMatrix1);
+        expectedMatrix.load(expectedMatrix1);
+        finalMult = multiply(multiply, testMatrix);
+
+        try {
+            if (isEqual(finalMult, expectedMatrix)) {
+                successMessage(1);
+                System.out.println(" Multiplication was successful");
+            }
+        } catch (Exception InvalidMatrixOperation) {
+            failMessage(1);
+            System.out.println(" Unsuccessful multiplication");
+        }
+
+        testMatrix = nullMatrices(testMatrix);
+        expectedMatrix = nullMatrices(expectedMatrix);
+
+        testMatrix.load(testMatrix2);
+        expectedMatrix.load(expectedMatrix2);
+        finalMult = multiply(multiply, testMatrix);
+
+        try {
+            if (isEqual(finalMult, expectedMatrix)) {
+                successMessage(2);
+                System.out.println(" Multiplication was successful");
+            }
+        } catch (Exception InvalidMatrixOperation) {
+            failMessage(2);
+            System.out.println(" Unsuccessful multiplication");
+        }
+
+        testMatrix = nullMatrices(testMatrix);
+        expectedMatrix = nullMatrices(expectedMatrix);
+
+        testMatrix.load(testMatrix3);
+        expectedMatrix.load(expectedMatrix3);
+        finalMult = multiply(multiply, testMatrix);
+
+        try {
+            if (isEqual(finalMult, expectedMatrix)) {
+                successMessage(3);
+                System.out.println(" Multiplication was successful");
+            }
+        } catch (Exception InvalidMatrixOperation) {
+            failMessage(3);
+            System.out.println(" Unsuccessful multiplication");
+        }
+
+        testMatrix = nullMatrices(testMatrix);
+        expectedMatrix = nullMatrices(expectedMatrix);
+
+        testMatrix.load(testMatrix4);
+        expectedMatrix.load(expectedMatrix4);
+        finalMult = multiply(multiply, testMatrix);
+
+        try {
+            if (isEqual(finalMult, expectedMatrix)) {
+                successMessage(4);
+                System.out.println(" Multiplication was successful");
+            }
+        } catch (Exception InvalidMatrixOperation) {
+            failMessage(4);
+            System.out.println(" Unsuccessful multiplication");
+        }
+
+        testMatrix = nullMatrices(testMatrix);
+        expectedMatrix = nullMatrices(expectedMatrix);
+
+        testMatrix.load(testMatrix5);
+        expectedMatrix.load(expectedMatrix5);
+        finalMult = multiply(multiply, testMatrix);
+
+        try {
+            if (isEqual(finalMult, expectedMatrix)) {
+                successMessage(5);
+                System.out.println(" Multiplication was successful");
+            }
+        } catch (Exception InvalidMatrixOperation) {
+            failMessage(5);
+            System.out.println(" Unsuccessful multiplication");
+        }
+
+        testMatrix = nullMatrices(testMatrix);
+        expectedMatrix = nullMatrices(expectedMatrix);
+
+        testMatrix.load(testMatrix6);
+        expectedMatrix.load(expectedMatrix6);
+        finalMult = multiply(multiply, testMatrix);
+
+        try {
+            if (isEqual(finalMult, expectedMatrix)) {
+                successMessage(6);
+                System.out.println(" Multiplication was successful");
+            }
+        } catch (Exception InvalidMatrixOperation) {
+            failMessage(6);
+            System.out.println(" Unsuccessful multiplication");
+        }
+
+        testMatrix = nullMatrices(testMatrix);
+        expectedMatrix = nullMatrices(expectedMatrix);
+
+        testMatrix.load(testMatrix7);
+        expectedMatrix.load(expectedMatrix7);
+        finalMult = multiply(multiply, testMatrix);
+
+        try {
+            if (isEqual(finalMult, expectedMatrix)) {
+                successMessage(7);
+                System.out.println(" Multiplication was successful");
+            }
+        } catch (Exception InvalidMatrixOperation) {
+            failMessage(7);
+            System.out.println(" Unsuccessful multiplication");
+        }
+
+        testMatrix = nullMatrices(testMatrix);
+        expectedMatrix = nullMatrices(expectedMatrix);
+
+        testMatrix.load(testMatrix8);
+        expectedMatrix.load(expectedMatrix8);
+        finalMult = multiply(multiply, testMatrix);
+
+        try {
+            if (isEqual(finalMult, expectedMatrix)) {
+                successMessage(8);
+                System.out.println(" Multiplication was successful");
+            }
+        } catch (Exception InvalidMatrixOperation) {
+            failMessage(8);
+            System.out.println(" Unsuccessful multiplication");
+        }
+
+        testMatrix = nullMatrices(testMatrix);
+
+        try {
+            multiply(multiply, testMatrix);
+            failMessage(9);
+            System.out.println(" Matrix is invalid");
+        } catch (Exception NullPointerException) {
+            successMessage(9);
+            System.out.println(" Matrix is null");
+        }
+        System.out.println("===========================================\n");
+    }
+
+
+    public static Matrix nullMatrices(Matrix a) {
+        return new Matrix();
+    }
+
+    public static void successMessage(int testNumber) {
+        System.out.print("Test " + testNumber + ": Success");
+    }
+
+    public static void failMessage(int testNumber) {
+        System.out.print("Test " + testNumber + ": Failed");
     }
 }
